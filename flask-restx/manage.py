77168ff788cea04f7697700b13a9ae4af6a7e043
Priyanshu Raj <62762913+priyanshu9888@users.noreply.github.com>
2023-10-28 11:44:30 +0530

    Add files via upload

---
+++ flask-restx/manage.py
@@ -0,0 +1,25 @@
+import os
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app.main import create_app, db
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def run():
+    app.run()
+
+
+if __name__ == '__main__':
+    manager.run()
